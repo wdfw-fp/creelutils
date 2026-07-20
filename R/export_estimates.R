e2b331f994a36f1f8ed90ed12d7231e7aebf51b8
Colt Holley <colt.holley@dfw.wa.gov>
2026-07-19 22:53:28 -0700

    feat: update 'export_estimates' fn
    Uses 'resolved_params' now. New arg for catch groups that gets passes to 'prep_export' fn.

--- R/export_estimates.R
+++ R/export_estimates.R
@@ -3,7 +3,8 @@
 #' Primary control function of the export, transform, and load (ETL) process. Takes standardized model outputs and user-input parameters to export the model estimates appropriately.
 #'
 #' @family ETL
-#' @param params User-input parameters defined during the model estimation process.
+#' @param resolved_params User-input parameters defined during the model estimation process.
+#' @param est_catch_groups Output of `resolve_catch_groups()`.
 #' @param analysis_lut Lookup table created during the model estimation process which stores a session-specific analysis_id key and metadata about the analysis.
 #' @param creel_estimates List object containing model estimates in a standardized format. Typically passed from 'transform_estimates' function.
 #' @param conn Database connection object. If NULL (default), a new connection will be established.
@@ -11,108 +12,87 @@
 #' @return ??
 #' @export
 #'
-export_estimates <- function(params, analysis_lut, creel_estimates, conn = NULL) {
+export_estimates <- function(resolved_params, est_catch_groups, analysis_lut, creel_estimates, conn = NULL) {
 
   # Connect to database and conditionally export
-  if(params$export == tolower("database")) {
-
-    # estimate_reviewers <- c()
-    #
-    # if (!Sys.info()["user"] %in% estimate_reviewers && params$data_grade == tolower("approved")) {
-    #   stop("Creel project leads may only upload estimates with a data_grade of 'provisional'.")
-    # }
-
-    #convert metadata to json. Automatically added to analysis_lut
-    analysis_lut <- json_conversion(type = "r_session", params, analysis_lut)
+  if(resolved_params$export == tolower("database")) {
 
     # Connect to database if connection not supplied in argument
     if (is.null(conn)) {
-    con <- establish_db_con()
+    con <- creelutils::connect_creel_db()
     } else {
       con <- conn # Use connection provided
     }
 
+    # Add additional metadata fields to analysis lut
+    analysis_lut <- finalize_analysis_lut(analysis_lut = analysis_lut, resolved_params = resolved_params, conn = con)
+
     #query database for UUIDs and reformat
-    creel_estimates_db <- prep_export(con, creel_estimates)
+    creel_estimates_db <- prep_export(conn = con, creel_estimates, catch_group_lut = est_catch_groups)
 
     ### write estimates to database ####
 
     #model_analysis_lut
     #determine if session analysis_id already exists in database model_analysis_lut table
     cat("\nVerifying that session 'analysis_id' does not exist in database before upload.")
-    analysis_id_check <- fetch_db_table(con, "creel", "model_analysis_lut") |>
-      dplyr::select("analysis_id", "analysis_name")
+    analysis_id_check <- fetch_db_table(con, "creel", "model_analysis_lut") |> dplyr::select("analysis_id")
 
     if (analysis_lut$analysis_id %in% analysis_id_check$analysis_id) {
       cat("\n")
       stop("\nAnalysis uuid already exists in the creel database. Review before proceeding.")
     } else { #analysis_id not already in database
 
-      # a pause, option to abort process
-      # proceed <- ask_for_confirmation()
-      proceed <- TRUE #bypass this control for now
-
-      if (proceed) { #Y = TRUE
-        cat("Continuing with upload...\n")
-        Sys.sleep(3)
-
         #evaluate export_tables parameter
-        if (params$export_tables == "total") {
+        if (resolved_params$export_tables == "total") {
 
           #write lut and total
           cat(paste0("Writing to model_analysis_lut table...  ","\u2713", "\n"))
-          write_lut(con, analysis_lut)
+          write_lut(conn = con, analysis_lut)
 
           cat(paste0("Writing to model_estimates_total table...  ","\u2713", "\n"))
-          write_total(con, creel_estimates_db)
+          write_total(conn = con, creel_estimates_db)
 
-        } else if (params$export_tables == "stratum") {
+        } else if (resolved_params$export_tables == "stratum") {
 
           #write lut and stratum
           cat(paste0("Writing to model_analysis_lut table...  ","\u2713", "\n"))
-          write_lut(con, analysis_lut)
+          write_lut(conn = con, analysis_lut)
 
           cat(paste0("Writing to model_estimates_stratum table...  ","\u2713", "\n"))
-          write_stratum(con, creel_estimates_db)
+          write_stratum(conn = con, creel_estimates_db)
 
-        } else if (params$export_tables == "both") {
+        } else if (resolved_params$export_tables == "both") {
 
           #write lut, total, and stratum
           cat(paste0("Writing to model_analysis_lut table...  ","\u2713", "\n"))
-          write_lut(con, analysis_lut)
+          write_lut(conn = con, analysis_lut)
 
           cat(paste0("Writing to model_estimates_total table...  ","\u2713", "\n"))
-          write_total(con, creel_estimates_db)
+          write_total(conn = con, creel_estimates_db)
 
           cat(paste0("Writing to model_estimates_stratum table...  ","\u2713", "\n"))
-          write_stratum(con, creel_estimates_db)
+          write_stratum(conn = con, creel_estimates_db)
 
         } else {
           cat("\nParameter export_tables must be either 'total', 'stratum', or 'both'.")
         }
-
-      } else {
-        # If confirmation = No
-        cat("Writing to database tables aborted.\n")
-        return(NULL)
-      }
     }
 
     #verify that estimates have been written as expected
     cat("\nUploading complete. Verifying session 'analysis_id' in database analysis look up table.")
-    confirm_db_upload(con, analysis_lut)
+    confirm_db_upload(conn = con, analysis_lut)
 
     #local export option
-  } else if (params$export == tolower("local")) {
+  } else if (resolved_params$export == tolower("local")) {
     #process for exporting ETL output tables locally for inspection prior to uploading to database
 
-    #convert metadata to json. Automatically added to analysis_lut
-    analysis_lut <- json_conversion(type = "script", params, analysis_lut)
-    analysis_lut <- json_conversion(type = "r_session", params, analysis_lut)
+    # Add additional metadata fields to analysis lut
+    # Project name and fishery name not changed to ids
+    analysis_lut <- finalize_analysis_lut(analysis_lut, resolved_params, conn = NULL)
 
     #project- and fishery-specific folder from CreelEstimates
     #could be more flexible and make folders where needed? for case of recreation of script on computer that did run analysis
-    write_directory <- paste0(getwd(), "/fishery_analyses/", params$project_name, "/", params$fishery_name,"/")
+    write_directory <- paste0(getwd(), "/fishery_analyses/", resolved_params$project_name, "/", resolved_params$fishery_name,"/")
 
     #write csv files to local working directory
     readr::write_csv(analysis_lut, file = paste0(write_directory,"analysis_lut.csv"))
@@ -123,7 +103,7 @@ export_estimates <- function(params, analysis_lut, creel_estimates, conn = NULL)
     cat("Standardized model estimate tables and analysis_lut saved to fishery folder on local computer.")
 
     #do not write out estimates
-  } else if (params$export == tolower("No")) {
+  } else if (resolved_params$export == tolower("No")) {
     #send message to user that no ETL actions were taken
     cat("\n\n")
     cat("Catch and effort estimates not exported.")
