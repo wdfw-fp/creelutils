f8ba26c90e8eaffe2811e9e1b5df9095df617213
Colt Holley <colt.holley@dfw.wa.gov>
2026-07-19 21:15:04 -0700

    fix: drop unused column

--- R/query_helpers.R
+++ R/query_helpers.R
@@ -116,7 +116,6 @@ fishery_catchgroups <- function(
     table  = "vw_model_catch_group",
     filter = filter
     ) |>
-    dplyr::select(-.data$combined_catch_group) |> #temporary, drop after view update
     dplyr::mutate(
       combined_catch_group = combine_catch_group(
         dplyr::pick(dplyr::everything()
