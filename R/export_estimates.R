46318fc091fa703fb12bc487d4941fd481747ea6
Colt Holley <colt.holley@dfw.wa.gov>
2026-07-23 08:41:38 -0700

    feat: overhaul of `export_estimates()` fn
    No longer opens a fallback connection to prod, unsafe for stale test connection used. Enforces options on `params$export` and `params$export_tables`. Uses a better pre-check for duplicate analysis ids. Uses `DBI::dbWithTransaction` to write to the lut and model estimate tables as a batch. Records and compares rowcounts between what is to be uploaded and what actually gets added, rolling back the transaction on all the tables if one has an incomplete upload (feature only on export_tables = "both" right now, which we use 99% of time).
    
    I disabled the local export option for now, which returns a simple message. The analysis script ETL chunk has a local export option that override this because of the conditional statement structure, need to refactor somewhere at address that.

--- R/export_estimates.R
+++ R/export_estimates.R
@@ -7,110 +7,111 @@
 #' @param est_catch_groups Output of `resolve_catch_groups()`.
 #' @param analysis_lut Lookup table created during the model estimation process which stores a session-specific analysis_id key and metadata about the analysis.
 #' @param creel_estimates List object containing model estimates in a standardized format. Typically passed from 'transform_estimates' function.
-#' @param conn Database connection object. If NULL (default), a new connection will be established.
+#' @param conn Connection to the creel database.
 #'
-#' @return ??
+#' @return Invisibly, NULL. Called for side effects.
 #' @export
 #'
-export_estimates <- function(resolved_params, est_catch_groups, analysis_lut, creel_estimates, conn = NULL) {
+export_estimates <- function(resolved_params, est_catch_groups, analysis_lut, creel_estimates, conn) {
 
-  # Connect to database and conditionally export
-  if(resolved_params$export == tolower("database")) {
+  # Normalize and validate params
+  export <- tolower(resolved_params$export)
+  export_tables <- tolower(resolved_params$export_tables)
+  export_types <- c("database", "local", "no")
+  export_tables_types <- c("both", "total", "stratum")
 
-    # Connect to database if connection not supplied in argument
-    if (is.null(conn)) {
-    con <- creelutils::connect_creel_db()
+  if (!export %in% export_types) {
+    cli::cli_abort("Invalid {.field params$export} entered. Valid export types: {export_types}")
+  }
+  # Export to database ---------------------------------------------------------------------------
+  if(export == "database") {
+
+    if (is.null(conn) || !DBI::dbIsValid(conn)) {
+      cli::cli_abort("{.arg conn} is not a valid, open database connection.")
+    }
+
+    if (!export_tables %in% export_tables_types) {
+      cli::cli_abort("Invalid {.field params$export_tables} entered. Valid export table types: {export_tables_types}")
+    }
+
+    # Add additional metadata fields to analysis lut
+    analysis_lut <- finalize_analysis_lut(analysis_lut = analysis_lut, resolved_params = resolved_params, conn = conn)
+
+    # Query database for lookup table UUIDs and drop human-readable fields
+    # fishery_name for fishery_id, est_cg for model_catch_group_id, etc.
+    creel_estimates_db <- prep_export(conn = conn, creel_estimates, catch_group_lut = est_catch_groups)
+
+    # Record row counts to validate upload against
+    intended <- c(
+      lut = nrow(analysis_lut),
+      total = nrow(creel_estimates_db$total),
+      stratum = nrow(creel_estimates_db$stratum))
+
+    ## Write estimates --------------------------------------------------------------------------
+    existing_id_check <- fetch_db_table(
+      conn = conn, schema =  "creel", table = "model_analysis_lut",
+      filter = glue::glue("analysis_id == '{analysis_lut$analysis_id}'")
+    )
+
+    if (nrow(existing_id_check) > 0) {
+      cli::cli_abort("Duplicate {.val {analysis_lut$analysis_id}} detected. Database upload aborted.")
     } else {
-      con <- conn # Use connection provided
+      if (export_tables == "total") { # write lut and total
+
+        DBI::dbWithTransaction(conn = conn, {
+          write_lut(conn = conn, analysis_lut)
+          write_total(conn = conn, creel_estimates_db$total)
+        })
+      } else if (export_tables == "stratum") { # write lut and stratum
+
+        DBI::dbWithTransaction(conn = conn, {
+          write_lut(conn = conn, analysis_lut)
+          write_stratum(conn = conn, creel_estimates_db$stratum)
+        })
+      } else if (export_tables == "both") {
+        # Write lut, total, and stratum
+        DBI::dbWithTransaction(conn = conn, {
+          n <- c(
+            lut = write_lut(conn = conn, analysis_lut),
+            total = write_total(conn = conn, creel_estimates_db$total),
+            stratum = write_stratum(conn = conn, creel_estimates_db$stratum)
+          )
+          # Verify row counts on each table before exiting transaction
+          if (!all(n == intended)) {
+            failed <- names(intended)[n != intended]
+            cli::cli_abort(c(
+              "Row count mismatch; transaction rolled back.",
+              "x" = "Mismatched table{?s}: {.field {failed}}.",
+              "i" = "Intended: {intended[failed]}. Failed attempt: {n[failed]}."
+            ))
+          }
+        })
+      }
     }
-
-    # Add additional metadata fields to analysis lut
-    analysis_lut <- finalize_analysis_lut(analysis_lut = analysis_lut, resolved_params = resolved_params, conn = con)
-
-    #query database for UUIDs and reformat
-    creel_estimates_db <- prep_export(conn = con, creel_estimates, catch_group_lut = est_catch_groups)
-
-    ### write estimates to database ####
-
-    #model_analysis_lut
-    #determine if session analysis_id already exists in database model_analysis_lut table
-    cat("\nVerifying that session 'analysis_id' does not exist in database before upload.")
-    analysis_id_check <- fetch_db_table(con, "creel", "model_analysis_lut") |> dplyr::select("analysis_id")
-
-    if (analysis_lut$analysis_id %in% analysis_id_check$analysis_id) {
-      cat("\n")
-      stop("\nAnalysis uuid already exists in the creel database. Review before proceeding.")
-    } else { #analysis_id not already in database
-
-        #evaluate export_tables parameter
-        if (resolved_params$export_tables == "total") {
-
-          #write lut and total
-          cat(paste0("Writing to model_analysis_lut table...  ","\u2713", "\n"))
-          write_lut(conn = con, analysis_lut)
-
-          cat(paste0("Writing to model_estimates_total table...  ","\u2713", "\n"))
-          write_total(conn = con, creel_estimates_db)
-
-        } else if (resolved_params$export_tables == "stratum") {
-
-          #write lut and stratum
-          cat(paste0("Writing to model_analysis_lut table...  ","\u2713", "\n"))
-          write_lut(conn = con, analysis_lut)
-
-          cat(paste0("Writing to model_estimates_stratum table...  ","\u2713", "\n"))
-          write_stratum(conn = con, creel_estimates_db)
-
-        } else if (resolved_params$export_tables == "both") {
-
-          #write lut, total, and stratum
-          cat(paste0("Writing to model_analysis_lut table...  ","\u2713", "\n"))
-          write_lut(conn = con, analysis_lut)
-
-          cat(paste0("Writing to model_estimates_total table...  ","\u2713", "\n"))
-          write_total(conn = con, creel_estimates_db)
-
-          cat(paste0("Writing to model_estimates_stratum table...  ","\u2713", "\n"))
-          write_stratum(conn = con, creel_estimates_db)
-
-        } else {
-          cat("\nParameter export_tables must be either 'total', 'stratum', or 'both'.")
-        }
-    }
-
-    #verify that estimates have been written as expected
-    cat("\nUploading complete. Verifying session 'analysis_id' in database analysis look up table.")
-    confirm_db_upload(conn = con, analysis_lut)
-
-    #local export option
-  } else if (resolved_params$export == tolower("local")) {
-    #process for exporting ETL output tables locally for inspection prior to uploading to database
-
-    # Add additional metadata fields to analysis lut
-    # Project name and fishery name not changed to ids
-    analysis_lut <- finalize_analysis_lut(analysis_lut, resolved_params, conn = NULL)
-
-    #project- and fishery-specific folder from CreelEstimates
-    #could be more flexible and make folders where needed? for case of recreation of script on computer that did run analysis
-    write_directory <- paste0(getwd(), "/fishery_analyses/", resolved_params$project_name, "/", resolved_params$fishery_name,"/")
-
-    #write csv files to local working directory
-    readr::write_csv(analysis_lut, file = paste0(write_directory,"analysis_lut.csv"))
-    readr::write_csv(creel_estimates$stratum, file = paste0(write_directory,"model_estimates_stratum.csv"))
-    readr::write_csv(creel_estimates$total, file = paste0(write_directory, "model_estimates_total.csv"))
-
-    cat("\n\n")
-    cat("Standardized model estimate tables and analysis_lut saved to fishery folder on local computer.")
-
-    #do not write out estimates
-  } else if (resolved_params$export == tolower("No")) {
-    #send message to user that no ETL actions were taken
-    cat("\n\n")
-    cat("Catch and effort estimates not exported.")
-    cat("\nStandardized model estimates can be viewed in output list object 'creel_estimates'.")
+  # Export locally ------------------------------------------------------------------------------
+  } else if (export == "local") {
+    # #process for exporting ETL output tables locally for inspection prior to uploading to database
+    #
+    # # Add additional metadata fields to analysis lut
+    # # Project name and fishery name not changed to ids
+    # analysis_lut <- finalize_analysis_lut(analysis_lut, resolved_params, conn = NULL)
+    #
+    # #project- and fishery-specific folder from CreelEstimates
+    # #could be more flexible and make folders where needed? for case of recreation of script on computer that did run analysis
+    # write_directory <- paste0(getwd(), "/fishery_analyses/", resolved_params$project_name, "/", resolved_params$fishery_name,"/")
+    #
+    # #write csv files to local working directory
+    # readr::write_csv(analysis_lut, file = paste0(write_directory,"analysis_lut.csv"))
+    # readr::write_csv(creel_estimates$stratum, file = paste0(write_directory,"model_estimates_stratum.csv"))
+    # readr::write_csv(creel_estimates$total, file = paste0(write_directory, "model_estimates_total.csv"))
+    #
+    # cat("\n\n")
+    # cat("Standardized model estimate tables and analysis_lut saved to fishery folder on local computer.")
+    cli::cli_inform("Local export option internal to {.fun export_estimates} is currently disabled
+                    in favor of local export option in analysis script.")
 
   } else {
-    #send message to user with correct export parameter options
-    cat("Export parameter must be either 'no', 'local', or 'database'.")
+    cli::cli_inform("{.field creel_estimates} not exported. Set {.field params$export} to {.q local} or {.q database}.")
   }
+  return(invisible(NULL))
 }
